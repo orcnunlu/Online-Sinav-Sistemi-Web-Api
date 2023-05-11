b5f0f304e22c33ab7372a2a1711bb301c74f22e7
Orçun Ünlü <118480028+orcnunlu@users.noreply.github.com>
2023-05-12 00:57:59 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                question1 = "Akdenizin bitki örtüsü nedir",
+                answer1="Savana",
+                answer2="Maki",
+                answer3="Tundra",
+                answer4="Orman"
+
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebApplication1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Primitives;
+
+namespace WebApplication1
+{
+    public class WeatherForecast
+    {
+        public string question1 { get; set; }
+        public string answer1 { get; set; }
+        public string answer2 { get; set; }
+        public string answer3 { get; set; }
+        public string answer4 { get; set; }
+    }
+}
